12372129ef408bc723e0e7dae6e205d7193b7a45
tariq2293 <tariq@provility.com>
2023-11-20 19:34:14 +0530

    test-java starting

---
+++ src/test/java/com/example/springbootjavatest/SpringbootJavaTestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.springbootjavatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SpringbootJavaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+        int a = 10;
+        int b = 20;
+        assertEquals(40, a+b);
+    }
+
+}
